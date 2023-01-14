c25a416e3bd423d7aaa3ad33cc529e33b7f69c11
Jay Stewart <jay.stewart451@gmail.com>
2023-01-14 15:10:57 -0500

    Changed theme and added button listener

--- app/src/main/java/com/example/cupholderjava/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cupholderjava/ui/home/HomeFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.cupholderjava.R;
 import com.example.cupholderjava.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -27,6 +29,14 @@ public class HomeFragment extends Fragment {
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Button resetButton = getView().findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(v -> System.out.println("Test") );
+
     }
 
     @Override
