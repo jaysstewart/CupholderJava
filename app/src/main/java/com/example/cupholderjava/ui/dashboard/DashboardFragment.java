b051c6dc6161109a1d527a20adc7f3af7ad4f4ee
Jay <jay.stewart451@gmail.com>
2023-01-17 12:08:17 -0500

    Created waterbottle vector and adding progress bar functionality

--- app/src/main/java/com/example/cupholderjava/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/cupholderjava/ui/dashboard/DashboardFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.cupholderjava.R;
 import com.example.cupholderjava.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
@@ -27,6 +30,13 @@ public class DashboardFragment extends Fragment {
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ProgressBar bar = getView().findViewById(R.id.waterBar);
+        bar.setProgress(10);
     }
 
     @Override
