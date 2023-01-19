fd0d6ffd1a2037589151744dd9d339c849f309dd
Jay <jay.stewart451@gmail.com>
2023-01-19 09:16:47 -0500

    precent text properly displays text, added weight text

--- app/src/main/java/com/example/cupholderjava/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/cupholderjava/ui/dashboard/DashboardFragment.java
@@ -8,9 +8,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.cupholderjava.R;
 import com.example.cupholderjava.databinding.FragmentDashboardBinding;
@@ -21,29 +19,24 @@ public class DashboardFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        ProgressBar waterBar = getView().findViewById(R.id.waterBar);
+        final ProgressBar waterBar = getView().findViewById(R.id.waterBar);
         waterBar.setProgress(30);
 
-        TextView fillPercent = getView().findViewById(R.id.fillPercent);
+        final TextView fillPercent = getView().findViewById(R.id.percent);
         CharSequence str = "30%";
         fillPercent.setText(str);
-
-
     }
+
 
     @Override
     public void onDestroyView() {
