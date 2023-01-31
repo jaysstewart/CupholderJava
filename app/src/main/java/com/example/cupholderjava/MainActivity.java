cedb291b2f626ab7ad867b22c34b0c85f22c1514
Jay Stewart <jay.stewart451@gmail.com>
2023-01-31 17:48:39 -0500

    Allow android device to be discoverable on bluetooth

--- app/src/main/java/com/example/cupholderjava/MainActivity.java
+++ app/src/main/java/com/example/cupholderjava/MainActivity.java
@@ -4,17 +4,29 @@ import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.example.cupholderjava.ui.dashboard.DashboardFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
@@ -26,13 +38,29 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.cupholderjava.databinding.ActivityMainBinding;
 
+import java.lang.reflect.Method;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     Button reset;
 
+    private BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Message msg = Message.obtain();
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                //Found, add to a device list
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         createNotificationChannel();
 
@@ -50,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
 
         reset = findViewById(R.id.resetButton);
-        reset.setOnClickListener(v -> createNotification("title", "content"));
+        reset.setOnClickListener(v -> bluetooth());
 
     }
 
@@ -74,5 +102,45 @@ public class MainActivity extends AppCompatActivity {
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
         managerCompat.notify(1, builder.build());
     }
+
+    @SuppressLint("MissingPermission")
+    public void bluetooth() {
+        BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        makeDiscoverable();
+        bluetoothAdapter.startDiscovery();
+
+        if (bluetoothAdapter == null) {
+            return;
+        }
+
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress();
+            }
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void makeDiscoverable() {
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+        Log.i("Log", "Discoverable ");
+    }
+
+    public boolean createBond(BluetoothDevice btDevice)
+            throws Exception
+    {
+        Class class1 = Class.forName("android.bluetooth.BluetoothDevice");
+        Method createBondMethod = class1.getMethod("createBond");
+        Boolean returnValue = (Boolean) createBondMethod.invoke(btDevice);
+        return returnValue.booleanValue();
+    }
 }
+
 
